879efdcb5527ef967148a05d961ae7c5ac7a9904
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-17 23:16:26 -0500

    Links bug and hero responsive improved

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
 const $burger = document.querySelector('.hamburger');
 const $linksMenu = document.querySelector('.header__navbar__links');
 
-$burger.addEventListener('click', event => {
-	$burger.classList.toggle('is-active');
-	$linksMenu.classList.toggle('active');
+document.addEventListener('click', event => {
+	if (
+		event.target.matches('.hamburger') ||
+		event.target.matches('.hamburger *')
+	) {
+		$burger.classList.toggle('is-active');
+		$linksMenu.classList.toggle('active');
+	}
+	if (event.target.matches('.header__navbar__links__item')) {
+		$burger.classList.remove('is-active');
+		$linksMenu.classList.remove('active');
+	}
 });
